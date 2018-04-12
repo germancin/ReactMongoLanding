05edabec8f5e6ab722d1fa9213ead9510150ce8c
german gonzalez <gcomlnk@gmail.com>
2018-04-12 09:28:13 -0400

    new file

--- landing/src/components/Leads.js
+++ landing/src/components/Leads.js
@@ -24,7 +24,40 @@ class Leads extends Component {
         this.props.deleteLeads(leadId);
     };
 
+    renderShowsTotal = (start, to, total)=> {
+        return (
+            <div style={ { color: 'black', textAlign:'left' } }>
+                From { start } to { to }, Total: { total }
+            </div>
+        );
+    };
+
     render() {
+
+        const options = {
+            page: 1,  // which page you want to show as default
+            sizePerPageList: [ {
+                text: '5', value: 5
+            }, {
+                text: '10', value: 10
+            }, {
+                text: 'All', value: this.props.leadsM.length
+            } ], // you can change the dropdown list for size per page
+            sizePerPage: 15,  // which size per page you want to locate as default
+            pageStartIndex: 1, // where to start counting the pages
+            paginationSize: 5,  // the pagination bar size.
+            prePage: 'Prev', // Previous page button text
+            nextPage: 'Next', // Next page button text
+            firstPage: 'First', // First page button text
+            lastPage: 'Last', // Last page button text
+            paginationShowsTotal: this.renderShowsTotal,  // Accept bool or function
+            paginationPosition: 'top',  // default is bottom, top and both is all available
+            hideSizePerPage: true, //> You can hide the dropdown for sizePerPage
+            // alwaysShowAllBtns: true // Always show next and previous button
+            // withFirstAndLast: false > Hide the going to First and Last page button
+            onDeleteRow:this.handleOnDeleteRow, // This is the hook for delete
+        };
+
         return (
             <LeadsContainer>
 
@@ -37,14 +70,14 @@ class Leads extends Component {
                             ?
                                 <Row className={'notes-box'}>
 
-
                                     <BootstrapTable data={this.props.leadsM}
                                                     multiColumnSearch={ true }
                                                     search={ true }
                                                     exportCSV={ true }
                                                     deleteRow={ true }
-                                                    selectRow={ {mode:'checkbox'} }
-                                                    options={ {onDeleteRow:this.handleOnDeleteRow} }
+                                                    selectRow={ {mode:'checkbox', bgColor:'lightgrey'} }
+                                                    pagination={ true }
+                                                    options={ options }
                                                     cellEdit={
                                                                 {
                                                                     mode:'click',
@@ -54,7 +87,7 @@ class Leads extends Component {
                                                               }
                                                     striped hover
                                     >
-                                        <TableHeaderColumn isKey dataField='_id'>Id</TableHeaderColumn>
+                                        <TableHeaderColumn isKey hidden dataField='_id'>Id</TableHeaderColumn>
                                         <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
                                         <TableHeaderColumn dataField='email'>Email</TableHeaderColumn>
                                         <TableHeaderColumn dataField='phone'>Phone</TableHeaderColumn>
