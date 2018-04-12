8b99c368485200ffdd430e0c23e36b176f3b2bed
german gonzalez <gcomlnk@gmail.com>
2018-04-12 02:59:59 -0400

    button click for ipad

--- landing/src/actions/index.js
+++ landing/src/actions/index.js
@@ -10,8 +10,8 @@ export const USER = 'USER';
 export const USER_INFO = 'USER_INFO';
 export const ERROR_SAVING_LEAD = 'ERROR_SAVING_LEAD';
 
-// const uri = 'http://208.68.36.212:3040';
-const uri = 'http://localhost:3040';
+const uri = 'http://208.68.36.212:3040';
+// const uri = 'http://localhost:3040';
 
 export const logOut = () => {
     const resp = axios.get(`http://localhost:3040/api/user/log_out`, {withCredentials: true});
@@ -57,12 +57,14 @@ export const getLeads = () => {
 
     const leads = axios.get(`${uri}/api/lead`);
 
-    alert(leads);
-
+    console.log('leads from get ledas::', leads);
     return dispatch => {
         dispatch({type: FETCHING, fetching: true});
         leads
             .then(response => {
+
+                console.log('ALL LEADS::::', response.data);
+
                 dispatch({type: GET_LEADS, payload: response.data.reverse(), first_time:false});
                 // dispatch({type: FETCHING, fetching: false});
             })
